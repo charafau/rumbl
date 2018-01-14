cfdd119e42e41b28aaaa52d4ef4a45637a6f004a
charafau <gorudonu@gmail.com>
2018-01-14 20:51:18 +0000

    adding hashing passwords with argon2

--- lib/rumbl_web/models/user.ex
+++ lib/rumbl_web/models/user.ex
@@ -19,4 +19,27 @@ defmodule Rumbl.Accounts.User do
     |> validate_length(:username, min: 1, max: 20)
   end
 
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_required([:password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{
+        valid?: true,
+        changes: %{
+          password: pass
+        }
+      } ->
+        put_change(changeset, :password_hash, Comeonin.Argon2.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
+  end
+
 end
