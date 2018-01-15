1a0eed9a1662b71786a972198f8b66bc84df2f21
charafau <gorudonu@gmail.com>
2018-01-15 23:09:08 +0000

    added login plug, added login after create

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -3,6 +3,8 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
+  plug :authenticate when action in [:index, :show]
+
   def index(conn, _params) do
     users = Repo.all(User)
     render conn, "index.html", users: users
@@ -23,10 +25,23 @@ defmodule RumblWeb.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "you must be logged in to access that page.")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
     end
 
   end
