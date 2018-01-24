6b084e64a2b42e5faa9e0ecd7c2bfb54b54ee713
charafau <gorudonu@gmail.com>
2018-01-24 02:51:39 +0000

    added slugs

--- lib/rumbl/video_context/video.ex
+++ lib/rumbl/video_context/video.ex
@@ -4,11 +4,12 @@ defmodule Rumbl.VideoContext.Video do
   alias Rumbl.VideoContext.Video
   alias Rumbl.VideoContext.Category
 
-
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
   schema "videos" do
     field :description, :string
     field :title, :string
     field :url, :string
+    field :slug, :string
     belongs_to :user, Rumbl.Accounts.User
     belongs_to :category, Rumbl.VideoContext.Category
 
@@ -20,6 +21,28 @@ defmodule Rumbl.VideoContext.Video do
     video
     |> cast(attrs, [:url, :title, :description])
     |> validate_required([:url, :title, :description])
+    |> slugify_title()
     |> assoc_constraint(:category)
   end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+
+  defimpl Phoenix.Param, for: Rumbl.Accounts.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
+  end
+
 end
